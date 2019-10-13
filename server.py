07dfb277aaf38c027ff21b51378e0fb2222e5716
Mat <me@sigpwr.de>
2019-10-13 13:01:33 +0200

    Add hooks to get logging caps and customization
    The hooks enable conditional logging of different aspects of the session.
    Also, we're able to mimick the exim4 output closer to real life

--- server.py
+++ server.py
@@ -1,5 +1,6 @@
 import asyncio
 import logging
+from datetime import datetime, timezone
 
 from aiosmtpd.controller import Controller
 from aiosmtpd.handlers import Sink
@@ -8,14 +9,35 @@ from aiosmtpd.smtp import SMTP as Server, syntax
 
 class CustomHostnameController(Controller):
     def factory(self):
-        return Server(self.handler, hostname='mailhon.serv.local', ident="Exim 420")
+        full_ident = 'ESMTP Exim 4.84_2 %s' % datetime.now(timezone.utc).strftime("%a, %d %b %Y %H:%M:%S %z")
+        return Server(self.handler, data_size_limit=52428800, hostname='mailhon.serv.local', ident=full_ident)
+
+class HoneyHandler:
+    async def handle_HELO(self, server, session, envelope, hostname):
+        print('Incoming hostname: %s' % hostname)
+        session.host_name = hostname
+        return '250 {}'.format(server.hostname)
+
+    async def handle_EHLO(self, server, session, envelope, hostname):
+        print('Incoming hostname: %s' % hostname)
+        session.host_name = hostname
+        await server.push('250-PIPELINING')
+        return '250 HELP'
+
+    async def handle_VRFY(self, server, session, envelope, address):
+        print('Client tried to verify address: %s' % address)
+
+    async def handle_DATA(self, server, session, envelope):
+        print('Message from %s' % envelope.mail_from)
+        print('Message for %s' % envelope.rcpt_tos)
+        print('Message data:\n')
+        print(envelope.content.decode('utf8', errors='replace'))
+        print('End of message')
+        return '250 Message accepted for delivery'
 
 
 async def amain(loop):
-    # TODO: Add custom handlers for DATA and stuff (i.e. RCP)
-    # see https://aiosmtpd.readthedocs.io/en/latest/aiosmtpd/docs/controller.html#using-the-controller
-    # and handlers: https://aiosmtpd.readthedocs.io/en/latest/aiosmtpd/docs/handlers.html#handler-hooks
-    cont = CustomHostnameController(Sink(),port=8025)
+    cont = CustomHostnameController(HoneyHandler(), hostname='', port=8026)
     cont.start()
 
 
